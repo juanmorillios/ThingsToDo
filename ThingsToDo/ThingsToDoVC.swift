cad7f7eae8126136fdee197422af7075a21f2cf1
Juan Morillo <juanmorillios@gmail.com>
2017-12-17 00:04:57 +0100

    Add new item functionality complete.

--- ThingsToDo/ThingsToDoVC.swift
+++ ThingsToDo/ThingsToDoVC.swift
@@ -19,8 +19,6 @@ class ThingsToDoVC: UITableViewController {
     }
     
     // MARK: Métodos DataSource
-    
-    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -32,7 +30,6 @@ class ThingsToDoVC: UITableViewController {
     }
     
     //MARK: Implementar Mñetodos del Delegado
-    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
@@ -41,11 +38,28 @@ class ThingsToDoVC: UITableViewController {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
         
-        
         tableView.deselectRow(at: indexPath, animated: true)
-        
         print("Seleccionado: \(itemArray[indexPath.row])")
     }
     
+    //MARK - add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New ThingsTodo Item ", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+           
+            self.itemArray.append(textField.text ?? "")
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new Item"
+            textField = alertTextField
+            print(alertTextField)
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
+
+
 
