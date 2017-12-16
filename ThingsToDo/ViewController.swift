8d4425e5a6e62efd4d03125c0607362e04f78a62
Juan Morillo <juanmorillios@gmail.com>
2017-12-16 23:09:43 +0100

    Initial Commit

---
+++ ThingsToDo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThingsToDo
+//
+//  Created by Juan Morillo on 16/12/17.
+//  Copyright © 2017 Juan Morillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
